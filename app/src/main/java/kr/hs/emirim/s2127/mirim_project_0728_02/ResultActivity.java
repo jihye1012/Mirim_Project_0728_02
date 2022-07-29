96ebcf8f3448b45c615ce483a6d643122fc492a4
jihye1012 <yjh0510122@gmail.com>
2022-07-29 10:22:53 +0900

    최다득표 추가

--- app/src/main/java/kr/hs/emirim/s2127/mirim_project_0728_02/ResultActivity.java
+++ app/src/main/java/kr/hs/emirim/s2127/mirim_project_0728_02/ResultActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
@@ -15,9 +16,27 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        TextView tvTitle = findViewById(R.id.text_title);
+        ImageView imgv=findViewById(R.id.imgv);
         Intent intent=getIntent();
         String[] imgName=intent.getStringArrayExtra("imgName");
         int[] voteCount = intent.getIntArrayExtra("voteCount");
+        int[] imgRes= {R.drawable.imgv01,R.drawable.imgv02,R.drawable.imgv03,R.drawable.imgv04,R.drawable.imgv05,R.drawable.imgv06,
+                R.drawable.imgv07,R.drawable.imgv08,R.drawable.imgv09};
+
+        int max=voteCount[0];
+        int maxIndex=0;
+        for(int i=0; i<imgName.length; i++){
+            if(voteCount[i]>max){
+                max=voteCount[i];
+                maxIndex=i;
+            }
+        }
+
+
+        tvTitle.setText(imgName[maxIndex]);
+        imgv.setImageResource(imgRes[maxIndex]);
+
         TextView[] tv=new TextView[imgName.length];
         int[] textId={R.id.text01, R.id.text02, R.id.text03,R.id.text04, R.id.text05, R.id.text06,
                 R.id.text07, R.id.text08, R.id.text09};
